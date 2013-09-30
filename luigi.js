8f4e320ba18e2d64326b36a9cf09b9269575c17f
bgoodson <braun.goodson@mail.rakuten.com>
2013-09-30 14:05:14 -0400

    added source

---
+++ luigi.js
@@ -0,0 +1,105 @@
+module.exports = {
+	server: null,
+	port: null,
+	debug: null,
+	parseIos: function (ios) {
+		for (var io in ios) {
+			if (this.debug) {
+				console.log('socket://'+io+' -> '+ios[io]);
+			}
+			this.server.io.route(io,(function(server,f){
+				console.log('New IO Route.');
+				return function (q) {
+					console.log('New IO Response.');
+					q.io.broadcast = server.io.broadcast;
+					f(q);
+					console.log('IO Response Fullfilled.');
+				}
+			}(this.server,ios[io])));
+		}
+	},
+	parsePosts: function (posts) {
+		for (var p in posts) {
+			if (this.debug) {
+				console.log('http://localhost:'+this.port+p+' -> '+posts[p]);
+			}
+			this.server.post(p,posts[p]);
+		}
+	},
+	parseGets: function (gets) {
+		for (var g in gets) {
+			if (this.debug) {
+				console.log('http://localhost:'+this.port+g+' -> '+gets[g]);
+			}
+			this.server.get(g,gets[g]);
+		}
+	},
+	parseRoutes: function(routes) {
+		if (routes.http) {
+			if (routes.http.get) {
+				this.parseGets(routes.http.get);
+			}
+			if (routes.http.post) {
+				this.parsePosts(routes.http.post);
+			}
+		}
+		if (routes.socket) {
+			this.parseIos(routes.socket);
+		}
+	},
+	plumbing: function (routes) {
+		if (routes.port) {
+			this.port = routes.port;
+		} else {
+			this.port = 10000;
+		}
+		if (routes.debug) {
+			this.debug = routes.debug;
+		} else {
+			this.debug = false;
+		}
+		this.server = require('express.io')();
+		this.server.http().io();
+		this.server.listen(this.port);
+		this.parseRoutes(routes);
+		return this;
+	}
+}
+
+/*
+
+	Usage:
+
+	var luigi = require('luigi');
+	luigi.plumbing({
+		port: 10000,
+		http: {
+			get: {
+				'/' : function (q,r) {
+					return r.send('<!doctype html><html><script src=\'socket.io/socket.io.js\'></script></html>');
+				},
+				'/echo' : function (q,r) {
+					return r.send({
+						echo : 'GET /echo'
+					});
+				}
+			},
+			post: {
+				'/echo' : function (q,r) {
+					return r.send({
+						echo : 'POST /echo'
+					});
+				}
+			}
+		},
+		socket: {
+			'unicast:echo' : function (q) {
+				console.log('Fullfilling IO Request.');
+				return q.io.emit('unicast:echo','unicast:echo');
+			},
+			'broadcast:echo' : function (q) {
+				return q.io.broadcast('broadcast:echo','broadcast:echo');
+			}
+		}
+	});
+*/
